ba75a87d61ab055d10efebbafd16e70cda9fdfe8
Nepcam <cameron.nepe@gmail.com>
2021-03-03 10:20:24 +1300

    create a post and get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
+const auth = require('../../middleware/auth');
+
+const Post = require('../../models/Posts');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route   GET api/posts
-// @desc    Test route
+// @desc    This is a Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Posts route'));
+// router.get('/', (req, res) => res.send('Posts route'));
+
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post('/',
+  [
+    auth,
+    [
+      check('text', 'Text is required').not().isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await (await User.findById(req.user.id)).isSelected('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
