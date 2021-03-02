e04b3cfc56929ff4136075e3343cd8d63ee63b31
Nepcam <cameron.nepe@gmail.com>
2021-03-03 12:12:32 +1300

    delete a comment from a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -198,5 +198,39 @@ router.post('/comment/:id',
     }
   });
 
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete a comment
+// @access  Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comment.find(comment => comment.id === req.params.comment_id);
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorised' });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+    post.comment.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+})
 
 module.exports = router;
